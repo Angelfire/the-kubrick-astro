7244233be178f48da497a00f7e16acbdabd9b255
Andres Bedoya <mortiis.angel@gmail.com>
2024-07-03 19:35:37 -0500

    Add more metadata

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,29 +3,29 @@ import { defineCollection, z } from 'astro:content'
 import { CATEGORIES } from '@/site.config'
 
 const blogCollection = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-		tags: z.array(z.string()).optional(),
-		category: z.enum(CATEGORIES),
-	}),
+  type: 'content',
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    category: z.enum(CATEGORIES),
+  }),
 })
 
 const pageCollection = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
-		description: z.string(),
+    description: z.string(),
   }),
 })
 
 export const collections = {
-	'blog': blogCollection,
-	'page': pageCollection
+  'blog': blogCollection,
+  'page': pageCollection
 }
