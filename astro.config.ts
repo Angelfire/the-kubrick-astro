3de8cb3353666ea1d5a4eef60e2b8c53656eff03
Andres Bedoya <mortiis.angel@gmail.com>
2024-07-05 10:33:39 -0500

    Add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,19 @@
-import mdx from "@astrojs/mdx"
-import tailwind from "@astrojs/tailwind"
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://the-kubrick-astro.vercel.app/",
-  integrations: [tailwind(), mdx({
+  integrations: [tailwind(), sitemap(), mdx({
     syntaxHighlight: "shiki"
   })],
   markdown: {
     shikiConfig: {
       theme: "one-dark-pro",
-      wrap: true,
-    },
-  },
-})
+      wrap: true
+    }
+  }
+});
